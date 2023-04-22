a2a6363541bed0dd65b7d53702a039fd4e3c9d6d
Jabed Bhuiyan <pitechbdinfo@gmail.com>
2023-04-22 07:38:44 +0600

    Update app.py
    Updated

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def init():
     global model
     global controlnet
 
-    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
+    controlnet = ControlNetModel.from_pretrained("lllyasviel/ControlNet-v1-1", torch_dtype=torch.float16)
     model = StableDiffusionControlNetPipeline.from_pretrained(
         "XpucT/Deliberate", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
     )
