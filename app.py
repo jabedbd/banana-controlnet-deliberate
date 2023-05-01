5fba57a76a3c42a94812093b2957de9a93ae3209
Jabed Bhuiyan <pitechbdinfo@gmail.com>
2023-05-01 23:15:51 +0600

    arcaneface-diffusion

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def init():
 
     controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
     model = StableDiffusionControlNetPipeline.from_pretrained(
-        "nitrosocke/Arcane-Diffusion", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
+        "michaelz/arcaneface-diffusion", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
     )
 
 
